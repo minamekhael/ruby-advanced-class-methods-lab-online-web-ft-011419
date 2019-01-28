e192298123d548e2f59e1414b9c9140c0240fdcf
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-28 03:44:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -63,8 +63,7 @@ class Song
     song.name = (name.split(" - ")[1].chomp(".mp3"))
     song.artist_name = (name.split(" - ")[0])
     @@all << song
-    song
-  end
+  end.song
 
   def save
     self.class.all << self
