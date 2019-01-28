30626ad142eb4f842e8092dd7ade37263b8443ca
Mina Mekhael <mina.mekhael@ymail.com>
2019-01-28 03:40:35 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,58 @@
 class Song
   attr_accessor :name, :artist_name
   @@all = []
+  
+  def self.create
+   song = self.new
+   song.save
+   song
+  end
+  
+  def self.new_by_name(name_of_song)
+    s = self.new
+    s.name = name_of_song
+    s
+  end
+  
+  def self.create_by_name(new_by_name)
+    s = self.new
+    s.name = new_by_name
+    s.save
+    s
+  end
+  
+  def self.find_by_name(name)
+    @@all.find{|person| person.name == name}
+  end
+  
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+  
+  def self.alphabetical
+    @@all.sort_by{|song| song.name}
+  end
+  
+  def self.new_from_filename(filename)
+    
+     row = filename
+
+       data = row.split(" - ")
+      artist_name = data[0]
+      song_name = data[1].gsub(".mp3", "")
+
+     song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+  end
 
   def self.all
     @@all
+  end
+  
+  def self.create_from_filename(name)
+    
   end
 
   def save
